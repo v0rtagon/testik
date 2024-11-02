9c5e6e7484374f5c8b7c3b58906b289cbe1d829f
v0rtagon <148217134+v0rtagon@users.noreply.github.com>
2024-11-02 19:12:30 +0200

    1
    1

---
+++ script.js
@@ -0,0 +1,14 @@
+function goToLevelPage(levelNumber) {
+    console.log('Clicked on level ' + levelNumber);
+    window.location.href = 'level' + levelNumber + '.html';
+}
+
+function goToTopPlayersPage() {
+    console.log('Clicked on Top Players');
+    window.location.href = 'top_players.html';
+}
+
+function goToTopPlatformersPage() {
+    console.log('Clicked on Top Platformers');
+    window.location.href = 'top_platformers.html';
+}
